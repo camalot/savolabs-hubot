b861db1c6528f6f581b081244b2d891a13a157a5
Ryan Conrad <camalot@gmail.com>
2015-11-23 10:29:50 -0600

    added imgur search

--- bin/local.cmd
+++ bin/local.cmd
@@ -2,4 +2,5 @@
 set EXPRESS_PORT=8181
 set HUBOT_BRAIN_USE_STORAGE_EMULATOR=true
 set HUBOT_LOG_LEVEL=debug
-npm install && node_modules\.bin\hubot.cmd --name "hubot" %* 
+:: set HUBOT_IMGUR_CLIENTID=
+npm install && node_modules\.bin\hubot.cmd --name "hubot" %*
